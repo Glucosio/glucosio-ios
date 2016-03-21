1e2b77d9c52282f11f626ef9375448a2a80b3b7a
Chris Walters <walters@id.com>
2016-03-21 08:56:51 +0000

    Be more thorough about finding localizations from the base localizations [touched: 7]

--- glucosio/Classes/Utiilities/Localization/GLUCLoc.m
+++ glucosio/Classes/Utiilities/Localization/GLUCLoc.m
@@ -2,13 +2,30 @@
 
 NSString *GLUCLoc(NSString *key)
 {
+    NSString *retVal = key;
     NSString *stringsTable = nil;
     NSString *preferredLocale = [[NSUserDefaults standardUserDefaults] valueForKey:kGLUCApplicationLocalePrefKey];
 
     if (preferredLocale) {
         stringsTable = [NSString stringWithFormat:kGLUCStringsTableTemplate, [preferredLocale lowercaseString]];
     }
-
-    return [[NSBundle mainBundle] localizedStringForKey:(key) value:key table:stringsTable];
+    
+    if (stringsTable) {
+        retVal = [[NSBundle mainBundle] localizedStringForKey:(key) value:key table:stringsTable];
+    } else {
+        retVal = NSLocalizedString(key, @"");
+        if ([retVal isEqualToString:key]) {
+            NSString *pathToBase = [[NSBundle mainBundle] pathForResource:@"Base" ofType:@"lproj"];
+            NSBundle *bundle = nil;
+            if (!pathToBase) {
+                bundle = [NSBundle mainBundle];
+            } else {
+                bundle = [NSBundle bundleWithPath:pathToBase];
+            }
+            if (bundle)
+                retVal = [bundle localizedStringForKey:(key) value:key table:stringsTable];
+        }
+    }
+    return retVal;
 }
 
