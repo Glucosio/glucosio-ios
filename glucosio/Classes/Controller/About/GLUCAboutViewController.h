a8bfbd64035201398dc4bcbf5ab1cb17214f894f
Chris Walters <walters@id.com>
2017-12-12 17:27:40 +0000

    Updated to use https for about/overview page

--- glucosio/Classes/Controller/About/GLUCAboutViewController.h
+++ glucosio/Classes/Controller/About/GLUCAboutViewController.h
@@ -1,7 +1,7 @@
 #import <UIKit/UIKit.h>
 #import "GLUCWebViewController.h"
 
-static NSString *const kGLUCDefaultHomePageURLString = @"http://www.glucosio.org/";
+static NSString *const kGLUCDefaultHomePageURLString = @"https://www.glucosio.org/";
 
 @interface GLUCAboutViewController : GLUCWebViewController
 
