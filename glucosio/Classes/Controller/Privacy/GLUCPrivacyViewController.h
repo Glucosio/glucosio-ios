cf67781b7337b762bafffd1eba226d297c310f11
Chris Walters <walters@id.com>
2017-12-12 17:26:23 +0000

    Updated to use https URL for privacy policy

--- glucosio/Classes/Controller/Privacy/GLUCPrivacyViewController.h
+++ glucosio/Classes/Controller/Privacy/GLUCPrivacyViewController.h
@@ -1,7 +1,7 @@
 #import <UIKit/UIKit.h>
 #import "GLUCWebViewController.h"
 
-static NSString *const kGLUCPrivacyUrlString = @"http://www.glucosio.org/privacy/";
+static NSString *const kGLUCPrivacyUrlString = @"https://www.glucosio.org/privacy/";
 
 @interface GLUCPrivacyViewController : GLUCWebViewController
 
